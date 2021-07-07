9514cc12f5d8f30b3c759004be2dec62a15f7e53
姜堃 <540172574@qq.com>
2021-07-07 14:29:29 +0800

    fix bug

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -2,7 +2,7 @@
   <div @click="toggle">Dialog 示例</div>
   <h1>示例1</h1>
   <div style="position: relative; z-index: 1">
-    <Button @click.native="toggle">toggle</Button>
+    <Button @click="toggle">toggle</Button>
     <Dialog
       v-model:visible="x"
       :closeOnClickOverlay="false"
@@ -18,15 +18,8 @@
       </template>
     </Dialog>
   </div>
-  <div
-    style="
-      position: relative;
-      z-index: 2;
-      width: 300px;
-      height: 300px;
-      background: red;
-    "
-  ></div>
+  <h1>示例2</h1>
+  <Button @click="showDialog">show</Button>
 </template>
 
 <script lang="ts">
@@ -42,10 +35,7 @@ export default {
   setup() {
     const x = ref(false);
     const toggle = () => {
-      console.log(x);
       x.value = !x.value;
-      console.log(x);
-      console.log(111);
     };
     const f1 = () => {
       return false;
